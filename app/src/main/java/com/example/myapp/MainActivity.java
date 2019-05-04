e5109bc3065871b347675be6bf355c4c00fe17b5
jacksonhakizimana <jackhanjackson@gmail.com>
2019-05-04 13:36:22 +0200

    first commit

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.myapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button bLogin,bRegister;
+    EditText etUsername,etEmail,etPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bLogin = (Button) findViewById(R.id.bLogin);
+        bRegister = (Button) findViewById(R.id.bRegister);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+
+
+        bRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bRegister:
+                startActivity(new Intent(this,Jackson.class));
+                break;
+        }
+
+    }
+}
